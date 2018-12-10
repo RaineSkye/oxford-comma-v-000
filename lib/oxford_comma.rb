2816d060290df68f0647492d3b888021a0653c49
kitte anderson <kitteanderson@gmail.com>
2018-12-10 01:12:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    #array.insert(-2, "and").join (", ")??? how to make this work also
+  case array.length
+  when 1
+    "#{array[0]}"
+  when 2
+    array [0..1].join(" and ")
+  else
+    array [0...-1].join(", ") << ", and #{array[-1]}"
+  end
 end
